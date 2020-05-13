3ef6b8a681ee5ee4ba2e1ef6b81ed87d99a095a2
Asadullah007 <mrasadullah0@gmail.com>
2020-05-13 08:06:33 +0500

    React practice

--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import Demo from './Demo'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(< Demo />,document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
