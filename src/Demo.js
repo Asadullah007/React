29e6958214f75056d1aecf549b225cb8bd0cb0b6
Asadullah007 <mrasadullah0@gmail.com>
2020-05-13 06:15:57 +0500

    React practice

---
+++ src/Demo.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Demo extends Component{
+    render(){
+        return <h1> Hello React </h1>
+    }
+}
+export default Demo;
